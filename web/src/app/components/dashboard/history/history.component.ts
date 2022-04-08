4fbfa73e8762a4e209fda848ab1af61a85b89023
Steven K Fisher <fisherstevenk@gmail.com>
2022-04-08 16:17:32 -0400

    remove test data from historical

--- web/src/app/components/dashboard/history/history.component.ts
+++ web/src/app/components/dashboard/history/history.component.ts
@@ -54,28 +54,9 @@ export class HistoryComponent extends NotificationComponent implements OnInit, O
         );
         this.walletLoaded = this.walletService.walletLoaded;
         if (this.walletLoaded === true) {
-            for (let i = 0; i < 77; ++i) {
-                const example = new HistoricalClaim({
-                    "stake_addr": "stake_test1urr8a6dy0zr2wte054xu7x9t63mnksh3gmr2vwfsj24upfcc9krzk",
-                    "payment_addr": "addr_test1qra78hxqu8pvyugm3uyw56xg05ryxcwp3ezqjtf73dmtrk7x0m56g7yx5uhjlf2deuv2h4rh8dp0z3kx5cunpy4tcznscs90gk",
-                    "policyid": "542b7ade184b6eea769f42d2d1f2902f366e0e9369b719d671e3d498",
-                    "tokenname": "61706578636f696e",
-                    "fingerprint": "asset1wpsl4pru06cnuf8xy8n2s0l0tpwfu6t0t8yxzj",
-                    "amount": "1",
-                    "contract_id": 1,
-                    "user_id": 1,
-                    "txhash": "2eee3be7ceec6402d37eb9758f028ab770de0ab82d8efe1a5895f75088579951",
-                    "invalid": null,
-                    "invalid_descr": null,
-                    "timestamp": "2022-04-08T19:04:22.646296Z",
-                    "updated_at": "2022-04-08T19:04:22.646296Z"
-                });
-                this.claimHistoryArray.push(example);
-            }
-            this.claimHistoryArray = [...this.claimHistoryArray];
-            //    this.restService.getRewardHistory()
-            //       .then(res => this.processHistory(res))
-            //      .catch(e => this.handleError(e));
+            this.restService.getRewardHistory()
+                .then(res => this.processHistory(res))
+                .catch(e => this.handleError(e));
         }
     }
 
