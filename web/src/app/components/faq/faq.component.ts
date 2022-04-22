338dd816052cff4c91c9991d5d7aa95d0512ad04
Steven K Fisher <fisherstevenk@gmail.com>
2022-04-22 12:38:22 -0400

    mobile is killing me.. but works now.

--- web/src/app/components/faq/faq.component.ts
+++ web/src/app/components/faq/faq.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject} from "@angular/core";
 import {Title} from "@angular/platform-browser";
 import {DOCUMENT} from "@angular/common";
+import {DisclosureButton} from "./disclosure-button";
 
 @Component({
     selector: 'faq',
@@ -9,8 +10,33 @@ import {DOCUMENT} from "@angular/common";
 })
 
 export class FaqComponent {
+    public buttons: Map<string, DisclosureButton> = new Map<string, DisclosureButton>();
 
-    constructor(@Inject(DOCUMENT) private document: any, public titleService: Title) {
+    constructor(@Inject(DOCUMENT) public document: any, public titleService: Title) {
         this.titleService.setTitle("FAQ");
+        setTimeout(() => {
+            while (this.document.getElementsByClassName("toggleButton").length === 0) {
+                this.delay(100);
+            }
+            const buttons = this.document.getElementsByClassName("toggleButton");
+            for (let i = 0; i < buttons.length; i++) {
+                const button = new DisclosureButton(buttons[i]);
+                this.buttons.set(button.id, button);
+            }
+        });
+    }
+
+    public delay(ms: number) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    public toggleComponent(id: string) {
+        this.buttons.forEach(button => {
+            if (button.id !== id) {
+                button.hideContent();
+            } else {
+                button.toggleExpand();
+            }
+        });
     }
 }
